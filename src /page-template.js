894a956466eabcd1f89bde97f081d35fd404cb6a
khanhlam90 <91096296+khanhlam90@users.noreply.github.com>
2021-12-03 00:43:16 -0800

    readme template.js setup

---
+++ src /page-template.js
@@ -0,0 +1,47 @@
+`
+# [ Placeholder - Your Project Title ]
+
+[ Placeholder - license badge ]
+
+## Description
+
+[ Placeholder - Description details go here ]
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+
+[ Placeholder - installation ]
+
+## Usage 
+
+[ Placeholder - usage ]
+
+## License
+
+This project is using the [ Placeholder - License ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath) ] license.
+
+## Contributing
+
+[ Placeholder - Contributing ]
+
+## Tests
+
+[ Placeholder - Test ]
+
+## Questions
+
+Please reach me using:
+
+<a href = 'mailto: [ Placeholder - My Email ]'> My Email </a>
+
+[My Github Account] (https://github.com/ [ Placeholder - My Github Account ] )
+
+`
